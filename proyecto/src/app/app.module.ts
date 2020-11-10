abeb1d0e24be22780c658cd812b4851dd9dba230
Manuel Rodríguez Chico <rodriguez.chman20@aulas.triana>
2020-11-10 10:30:19 +0100

    Listas y formularios

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './componets/correo/correo.component';
+import { ListaCorreosComponent } from './componets/lista-correos/lista-correos.component';
+import { NuevoCorreoComponent } from './componets/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CorreoComponent
+    CorreoComponent,
+    ListaCorreosComponent,
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
